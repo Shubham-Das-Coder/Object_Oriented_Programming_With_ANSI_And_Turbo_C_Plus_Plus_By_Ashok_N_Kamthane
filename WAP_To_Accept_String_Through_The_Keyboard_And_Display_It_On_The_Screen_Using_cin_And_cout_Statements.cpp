9e2d411babacfa684cff637befd4373a7b4619a5
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-07 14:58:55 +0530

    Add files via upload

---
+++ WAP_To_Accept_String_Through_The_Keyboard_And_Display_It_On_The_Screen_Using_cin_And_cout_Statements.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+main()
+{
+    char name[15];
+    cout << "Enter your name : ";
+    cin >> name;
+    cout << "Your name is " << name;
+    return 0;
+}
