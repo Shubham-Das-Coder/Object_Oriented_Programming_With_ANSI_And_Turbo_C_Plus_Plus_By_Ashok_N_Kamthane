2e94d1c4997753dd5c4816e58a5498092280c520
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-07 15:05:01 +0530

    Add files via upload

---
+++ WAP_To_Read_Two_Integers_And_Display_Them_Using_cin_And_cout_Statements.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+main()
+{
+    int num, num1;
+    cout << "Enter Two Numbers : ";
+    cin >> num >> num1;
+    cout << "Entered Numbers Are : ";
+    cout << num << "\t" << num1;
+    return 0;
+}
