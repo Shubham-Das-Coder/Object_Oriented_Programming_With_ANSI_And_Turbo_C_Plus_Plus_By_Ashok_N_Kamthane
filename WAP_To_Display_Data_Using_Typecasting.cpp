1e7a977ae31334c2fa280d66fedbd5ced0b7c76f
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-07 18:41:48 +0530

    Add files via upload

---
+++ WAP_To_Display_Data_Using_Typecasting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int x = 77;
+    float y = 5.1252;
+    char z = 'A';
+    char city[15];
+    cout << "x = " << (char)x << endl;
+    cout << "y = " << (int)y << endl;
+    cout << "z = " << (int)z << endl;
+}
