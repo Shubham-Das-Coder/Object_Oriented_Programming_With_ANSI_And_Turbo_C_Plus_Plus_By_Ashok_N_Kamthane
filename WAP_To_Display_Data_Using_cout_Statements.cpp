fdae3ee2f86a63f8d5e3c68e63091ebc48c31951
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-07 15:14:53 +0530

    Add files via upload

---
+++ WAP_To_Display_Data_Using_cout_Statements.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "==================";
+    cout << endl;
+    cout << "Hello"; // Prints Hello
+    cout << "\n";
+    cout << 123; // Prints 123
+    cout << endl
+         << 3.145; // Prints 3.145
+    cout << endl
+         << "NUMBER : "
+         << "\t" << 452; // Prints string and value
+    cout << "\n==== The end ====";
+}
