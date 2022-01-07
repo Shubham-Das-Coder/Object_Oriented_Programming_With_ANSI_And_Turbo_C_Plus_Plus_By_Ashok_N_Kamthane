42a91141def77e76ef83622df301e31bc475d7a6
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-07 16:19:02 +0530

    Add files via upload

---
+++ WAP_To_Display_integer_float_And_string_Using_cout_Statement.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int x = 5;
+    float y = 5.2;
+    char z = 'z';
+    char city[] = "NANDED";
+
+    cout << "x = " << x << " y = " << y << " z = " << z << endl;
+    cout << "City = " << city;
+}
