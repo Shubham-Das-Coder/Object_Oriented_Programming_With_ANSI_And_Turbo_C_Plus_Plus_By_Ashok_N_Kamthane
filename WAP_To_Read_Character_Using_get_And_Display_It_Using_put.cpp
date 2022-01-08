b53f005647c1c06b7e0fb1eccbae8ad61f9b60a3
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-08 19:25:44 +0530

    Add files via upload

---
+++ WAP_To_Read_Character_Using_get_And_Display_It_Using_put.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout << "\nEnter a character : ";
+    cin.get(ch);
+    cout << "\nEntered character was : ";
+    cout.put(ch);
+}
