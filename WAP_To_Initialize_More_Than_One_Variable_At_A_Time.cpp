f62507da9e0eb86e978ecfc8d1155ec39a39d318
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-09 11:47:43 +0530

    Add files via upload

---
+++ WAP_To_Initialize_More_Than_One_Variable_At_A_Time.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    a = b = c = d = 5;
+    cout << "\na = " << a << "\nb = " << b << "\nc = " << c << "\nd = " << d;
+}
