619de8bd7c1e2125f8c2dc9f6279669fd0953cc4
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-09 11:31:30 +0530

    Add files via upload

---
+++ WAP_To_Read_Two_Integers_Through_The_Keyboard_And_Declare_The_Variables_In_C++_Style.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+main()
+{
+    cout << "Enter two numbers : ";
+    int num;
+    cin >> num;
+    int num1;
+    cin >> num1;
+    cout << "Entered numbers are : ";
+    cout << num << "\t" << num1;
+    return 0;
+}
