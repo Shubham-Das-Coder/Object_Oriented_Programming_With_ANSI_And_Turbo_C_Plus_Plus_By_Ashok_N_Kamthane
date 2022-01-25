55d11cf6b220b52c2c137cf8932f9ca2583951f2
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-25 16:19:34 +0530

    Add files via upload

---
+++ WAP_In_C_Plus_Plus_To_Demonstrate_Dynamic_Initialization.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\nEnter radius : ";
+    int r;
+    cin >> r;
+    float area = 3.14 * r * r;
+    cout << "\nArea = " << area;
+}
