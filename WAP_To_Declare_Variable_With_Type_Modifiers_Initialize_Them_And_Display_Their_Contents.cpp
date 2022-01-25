1e8122bb6754c687f7d195ecbad81b92cafb8348
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-01-25 18:55:01 +0530

    Add files via upload

---
+++ WAP_To_Declare_Variable_With_Type_Modifiers_Initialize_Them_And_Display_Their_Contents.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short t = 1;
+    long k = 54111;
+    unsigned u = 10;
+    signed j = -10;
+
+    cout << "\nt = " << t;
+    cout << "\nk = " << k;
+    cout << "\nu = " << u;
+    cout << "\nj = " << j;
+}
