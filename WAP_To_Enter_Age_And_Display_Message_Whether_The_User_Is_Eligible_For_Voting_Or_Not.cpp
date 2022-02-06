cdda0b4611c320e5d0aaf4aa99024b6d411debc5
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2022-02-06 18:12:09 +0530

    Add files via upload

---
+++ WAP_To_Enter_Age_And_Display_Message_Whether_The_User_Is_Eligible_For_Voting_Or_Not.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Enter Your Age : ";
+    cin >> age;
+    if (age >= 18)
+    {
+        cout << "You are eligible for voting";
+    }
+}
